b4d98727c2ef25c312d33a31f081673ff8609c58
Henrique Sarhan <sarhansalomao@gmail.com>
2023-07-17 17:56:13 -0300

    styling

--- mb-sbc/src/routes/+page.svelte
+++ mb-sbc/src/routes/+page.svelte
@@ -8,7 +8,7 @@
 <div class="container grid grid-flow-row lg:grid-flow-col lg:grid-cols-2 gap-0 lg:mt-[5vh] mx-auto bg-surface-50 overflow-hidden">
 	<div class="relative">
 		<div class="grid gap-1 absolute top-3 left-3 z-40">
-			<img src="mb-sbc-flags.png" alt="Admisson" class="w-16 h-auto lg:w-20">
+			<img src="mb-sbc-flags.png" alt="Admisson" class="w-32 h-auto lg:w-40">
 		</div>
 		<div class="grid gap-1 absolute top-0 right-0 lg:-right-12 2xl:-right-16 z-50">
 			<img src="mb-sbc-admission.png" alt="Admisson" class="w-24 h-24 2xl:w-32 2xl:h-32">
