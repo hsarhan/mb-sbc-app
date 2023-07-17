0b446c13faba1c195c873f1ce2451ced29944f81
Henrique Sarhan <sarhansalomao@gmail.com>
2023-07-17 14:49:57 -0300

    adapter

--- mb-sbc/svelte.config.js
+++ mb-sbc/svelte.config.js
@@ -1,12 +1,6 @@
 import adapter from '@sveltejs/adapter-netlify';
-import { vitePreprocess } from '@sveltejs/kit/vite';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-
+export default {
 	kit: {
 		// default options are shown
 		adapter: adapter({
@@ -21,5 +15,3 @@ const config = {
 		})
 	}
 };
-
-export default config;
