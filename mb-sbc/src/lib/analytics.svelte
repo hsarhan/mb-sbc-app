fe073173f08d2ee503bc30b48dbd371f9a60a9a6
Henrique Sarhan <sarhansalomao@gmail.com>
2023-07-25 15:34:06 -0300

    analytics

---
+++ mb-sbc/src/lib/analytics.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import { page } from '$app/stores'
+
+  let mID = 'G-KGHJT8ZS6J';
+  //GTM-WS9QRR4
+
+  $: {
+    if (typeof gtag !== 'undefined') {
+      gtag('config', mID, {
+        page_title: document.title,
+        page_path: $page.url.pathname,
+      })
+    }
+  }
+</script>
+
+<svelte:head>
+  <script
+    async
+    src="https://www.googletagmanager.com/gtag/js?id={mID}">
+  </script>
+  <script>
+    window.dataLayer = window.dataLayer || []
+
+    function gtag() {
+      dataLayer.push(arguments)
+    }
+
+    gtag('js', new Date())
+    gtag('config', {mID})
+  </script>
+</svelte:head>
--- mb-sbc/src/routes/+layout.svelte
+++ mb-sbc/src/routes/+layout.svelte
@@ -5,10 +5,13 @@
 	// Most of your app wide CSS should be put in this file
 	import '../app.postcss';
 	import { AppShell } from '@skeletonlabs/skeleton';
+	import Analytics from '$lib/analytics.svelte';
 </script>
 
 <!-- App Shell -->
 
 <AppShell>
+	<Analytics />
+
 	<slot />
 </AppShell>
