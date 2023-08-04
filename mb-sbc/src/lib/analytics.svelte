0e7ffc6b6dbeabec89e9277683b0658422f48c12
Henrique Sarhan <sarhansalomao@gmail.com>
2023-08-04 15:08:42 -0300

    SA GTM

--- mb-sbc/src/lib/analytics.svelte
+++ mb-sbc/src/lib/analytics.svelte
@@ -1,35 +1,46 @@
 <script lang="ts">
-  import { page } from '$app/stores'
-  import { GoogleAnalytics } from '@beyonk/svelte-google-analytics'
+	import { page } from '$app/stores';
+	import { GoogleAnalytics } from '@beyonk/svelte-google-analytics';
 
-  let mID = 'G-KGHJT8ZS6J';
-  //G-KGHJT8ZS6J, ...GTM-WS9QRR4
+	let mID = 'G-KGHJT8ZS6J';
+	//G-KGHJT8ZS6J, ...GTM-WS9QRR4
 
-  $: {
-    if (typeof gtag !== 'undefined') {
-      gtag('config', mID, {
-        page_title: document.title,
-        page_path: $page.url.pathname,
-      })
-    }
-  }
+	$: {
+		if (typeof gtag !== 'undefined') {
+			gtag('config', mID, {
+				page_title: document.title,
+				page_path: $page.url.pathname
+			});
+		}
+	}
 </script>
 
 <svelte:head>
-  <script
-    async
-    src="https://www.googletagmanager.com/gtag/js?id={mID}">
-  </script>
-  <script>
-    window.dataLayer = window.dataLayer || []
+	<script>
+		(function (w, d, s, l, i) {
+			w[l] = w[l] || [];
+			w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
+			var f = d.getElementsByTagName(s)[0],
+				j = d.createElement(s),
+				dl = l != 'dataLayer' ? '&l=' + l : '';
+			j.async = true;
+			j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+			f.parentNode.insertBefore(j, f);
+		})(window, document, 'script', 'dataLayer', 'GTM-WS9QRR4');
+	</script>
 
-    function gtag() {
-      dataLayer.push(arguments)
-    }
+	<script async src="https://www.googletagmanager.com/gtag/js?id={mID}">
+	</script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
 
-    gtag('js', new Date())
-    gtag('config', {mID})
-  </script>
+		function gtag() {
+			dataLayer.push(arguments);
+		}
+
+		gtag('js', new Date());
+		gtag('config', { mID });
+	</script>
 </svelte:head>
 
-<GoogleAnalytics properties={[ mID ]} />
+<GoogleAnalytics properties={[mID]} />
--- mb-sbc/src/lib/components/Form.svelte
+++ mb-sbc/src/lib/components/Form.svelte
@@ -1,9 +1,9 @@
 <script>
 	import { fly, fade } from 'svelte/transition';
-	import { ga } from '@beyonk/svelte-google-analytics'
+	import { ga } from '@beyonk/svelte-google-analytics';
 	import Thanks from './Thanks.svelte';
 
-	let submitStatus = "";
+	let submitStatus = '';
 	let hasError = false;
 	let isSuccessVisible = false;
 	let isFormVisible = true;
@@ -17,7 +17,6 @@
 		ga.addEvent('form_success', {
 			lead: 'captured'
 		});
-
 	};
 
 	const submitForm = async (data) => {
@@ -45,48 +44,56 @@
 			tks = true;
 		}, 3750);
 	};
-
 </script>
-<div class="grid gap-4 lg:gap-12 content-between lg:content-center md:content-start bg-surface-100 dark:bg-primary-700 p-8 lg:px-12 xl:px-16 aspect-square md:aspect-auto lg:aspect-square">
-    {#if tks}
-		<div in:fade={{ duration: 500}} out:fade={{ duration: 450, delay: 500 }}>
+
+<div
+	class="grid gap-4 lg:gap-12 content-between lg:content-center md:content-start bg-surface-100 dark:bg-primary-700 p-8 lg:px-12 xl:px-16 aspect-square md:aspect-auto lg:aspect-square"
+>
+	{#if tks}
+		<div in:fade={{ duration: 500 }} out:fade={{ duration: 450, delay: 500 }}>
 			<Thanks />
 		</div>
 	{:else}
-	<h2
-        class="text-gray-500 dark:text-surface-50 font-light text-3xl lg:text-4xl 2xl:text-6xl mb-4 max-w-[75vw] md:max-w-full lg:max-w-[25ch] xl:max-w-full"
-    >
-        Conheça e se apaixone pelo nosso espaço e nossa metodologia
-    </h2>
-    <div class="">
-        <div class="text-gray-500 grid">
-			{#if hasError == true}
-				<p class="text-center text-error-800 border border-error-800 rounded-sm p-2 mb-4">
-					{errMessage}
-				</p>
-			{:else if isSuccessVisible}
-				<p
-					class="text-center text-white bg-secondary-500 border border-secondary-500 rounded-sm p-2 mb-4"
-					in:fly={{ y: -32, duration: 2000, delay: 500 }}
-					out:fade={{ duration: 450, delay: 500 }}
-				>
-					Dados enviados com sucesso. Obrigado!
-				</p>
-			{/if}
+		<h2
+			class="text-gray-500 dark:text-surface-50 font-light text-3xl lg:text-4xl 2xl:text-6xl mb-4 max-w-[75vw] md:max-w-full lg:max-w-[25ch] xl:max-w-full"
+		>
+			Conheça e se apaixone pelo nosso espaço e nossa metodologia
+		</h2>
+		<div class="">
+			<div class="text-gray-500 grid">
+				{#if hasError == true}
+					<p class="text-center text-error-800 border border-error-800 rounded-sm p-2 mb-4">
+						{errMessage}
+					</p>
+				{:else if isSuccessVisible}
+					<p
+						class="text-center text-white bg-secondary-500 border border-secondary-500 rounded-sm p-2 mb-4"
+						in:fly={{ y: -32, duration: 2000, delay: 500 }}
+						out:fade={{ duration: 450, delay: 500 }}
+					>
+						Dados enviados com sucesso. Obrigado!
+					</p>
+				{/if}
 
-			{#if submitStatus == 'submitting'}
-				<div class="grid grid-cols-[auto_1fr] align-middle justify-start pb-2">
-					<p>Enviando...</p>
-				</div>
-			{:else if submitStatus == 'failed'}
-				<p>Erro! Dados não enviados</p>
-			{:else if submitStatus == 'success'}
-				<p>Dados enviados com sucesso. Obrigado!</p>
-			{:else}
-				{#if isFormVisible}
-					<form transition:fade on:submit|preventDefault={submitForm} class="grid gap-4 md:grid-cols-2 dark:text-surface-50" id="admission">
+				{#if submitStatus == 'submitting'}
+					<div class="grid grid-cols-[auto_1fr] align-middle justify-start pb-2">
+						<p>Enviando...</p>
+					</div>
+				{:else if submitStatus == 'failed'}
+					<p>Erro! Dados não enviados</p>
+				{:else if submitStatus == 'success'}
+					<p>Dados enviados com sucesso. Obrigado!</p>
+				{:else if isFormVisible}
+					<form
+						transition:fade
+						on:submit|preventDefault={submitForm}
+						class="grid gap-4 md:grid-cols-2 dark:text-surface-50"
+						id="admission"
+					>
 						<div class="grid gap-2">
-							<label for="name" class="text-xs">Seu Nome <span class="text-slate-400 dark:text-slate-300">• Your name</span></label>
+							<label for="name" class="text-xs"
+								>Seu Nome <span class="text-slate-400 dark:text-slate-300">• Your name</span></label
+							>
 							<input
 								type="text"
 								name="name"
@@ -98,7 +105,11 @@
 						</div>
 
 						<div class="grid gap-2">
-							<label for="child" class="text-xs">Nome da criança <span class="text-slate-400 dark:text-slate-300">• Child's name</span></label>
+							<label for="child" class="text-xs"
+								>Nome da criança <span class="text-slate-400 dark:text-slate-300"
+									>• Child's name</span
+								></label
+							>
 							<input
 								type="text"
 								name="child"
@@ -110,22 +121,22 @@
 						</div>
 
 						<div class="grid gap-2">
-							<label for="grade" class="text-xs">Escolaridade <span class="text-slate-400 dark:text-slate-300">• Grade</span></label>				
-							
-							<select 
-								name="grade"
-								aria-label="grade"
-								class="input text-sm" >
+							<label for="grade" class="text-xs"
+								>Escolaridade <span class="text-slate-400 dark:text-slate-300">• Grade</span></label
+							>
+
+							<select name="grade" aria-label="grade" class="input text-sm">
 								<option value="Educação Infantil">Educação Infantil • Preschool</option>
 								<option value="Ensino Fundamental I">Ensino Fundamental I • Elementary</option>
 								<option value="Ensino Fundamental II">Ensino Fundamental II • Middle Years</option>
 								<option value="Ensino Médio">Ensino Médio • High School</option>
 							</select>
-								
 						</div>
 
 						<div class="grid gap-2">
-							<label for="email" class="text-xs">Seu e-mail <span class="text-slate-400 dark:text-slate-300">• E-mail</span></label>
+							<label for="email" class="text-xs"
+								>Seu e-mail <span class="text-slate-400 dark:text-slate-300">• E-mail</span></label
+							>
 							<input
 								type="email"
 								name="email"
@@ -137,7 +148,9 @@
 						</div>
 
 						<div class="grid gap-2">
-							<label for="phone" class="text-xs">Seu telefone <span class="text-slate-400 dark:text-slate-300">• Phone</span></label>
+							<label for="phone" class="text-xs"
+								>Seu telefone <span class="text-slate-400 dark:text-slate-300">• Phone</span></label
+							>
 							<input
 								name="phone"
 								type="text"
@@ -149,14 +162,16 @@
 						</div>
 
 						<div class="pt-3 md:pt-0 grid gap-2">
-							<button type="submit" on:click={() => (submitted = true)} class="mt-6 btn rounded-md font-semibold text-white bg-primary-400 dark:bg-primary-500"
+							<button
+								type="submit"
+								on:click={() => (submitted = true)}
+								class="mt-6 btn rounded-md font-semibold text-white bg-primary-400 dark:bg-primary-500"
 								>Enviar <span class="text-slate-100 font-normal ml-1"> • Send</span></button
 							>
 						</div>
 					</form>
 				{/if}
-			{/if}
-        </div>
-    </div>
+			</div>
+		</div>
 	{/if}
 </div>
